9ef946d6eb631e4921c2fc915c137d6503345d0d
Sebastien Aucante <sebastien.aucante@gmail.com>
2025-08-22 21:42:17 +0200

    feat(glg204): Stats DTOs

---
+++ src/main/java/com/glg204/fep/application/DashboardApplication/RefundStatsDTO.java
@@ -0,0 +1,40 @@
+package com.glg204.fep.application.DashboardApplication;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class RefundStatsDTO {
+    private List<MonthlyRefund> monthlyRefunds;
+    private int pendingRefundsAmount;
+    private int expectedRevenueNextMonth;
+    private List<UpcomingRefund> upcomingRefunds;
+    private List<LateRefund> lateRefunds;
+
+    @Data @Builder
+    public static class MonthlyRefund {
+        private String month;
+        private int refundedAmount;
+        private int expectedAmount;
+        private int lateAmount;
+        private int unpaidAmount;
+    }
+
+    @Data @Builder
+    public static class UpcomingRefund {
+        private String dueDate;
+        private int amount;
+        private String borrower;
+    }
+
+    @Data @Builder
+    public static class LateRefund {
+        private int refundId;
+        private int daysLate;
+        private int amount;
+        private String borrower;
+    }
+}
