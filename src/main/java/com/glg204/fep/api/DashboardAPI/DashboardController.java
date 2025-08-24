c500e1d50e99dfac586ccc7a9465d7ac2d5e628c
Sebastien Aucante <sebastien.aucante@gmail.com>
2025-08-24 23:10:17 +0200

    feat(glg204): Stats Endpoints

--- src/main/java/com/glg204/fep/api/DashboardAPI/DashboardController.java
+++ src/main/java/com/glg204/fep/api/DashboardAPI/DashboardController.java
@@ -1,7 +1,6 @@
 package com.glg204.fep.api.DashboardAPI;
 
-import com.glg204.fep.application.DashboardApplication.DashboardService;
-import com.glg204.fep.application.DashboardApplication.DashboardStatsDTO;
+import com.glg204.fep.application.DashboardApplication.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +16,26 @@ public class DashboardController {
     @GetMapping
     public DashboardStatsDTO getDashboard() {
         return dashboardService.getDashboardStats();
+    }
+
+    @GetMapping("/stats/global")
+    public GlobalStatsDTO getGlobalStats() {
+        return dashboardService.getGlobalStats();
+    }
+
+    @GetMapping("/stats/financial")
+    public FinancialStatsDTO getFinancialStats() {
+        return dashboardService.getFinancialStats();
+    }
+
+    @GetMapping("/stats/refunds")
+    public RefundStatsDTO getRefundStats() {
+        return dashboardService.getRefundStats();
+    }
+
+    @GetMapping("/stats/system")
+    public SystemStatsDTO getSystemStats() {
+        return dashboardService.getSystemStats();
     }
 }
 
