3661dc29da48dbc4d25e7ff7387d187db51171c8
Sebastien Aucante <sebastien.aucante@gmail.com>
2025-08-25 21:18:38 +0200

    feat(glg204): Fix data volume docker

--- src/main/java/com/glg204/fep/infrastructure/config/DataLoader.java
+++ src/main/java/com/glg204/fep/infrastructure/config/DataLoader.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Random;
 
 @Component
-@Profile("dev")
+@Profile({"dev", "docker"})
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
 
